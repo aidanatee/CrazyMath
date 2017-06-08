e5dadf0b33a4087b5a2449e0dcdfda2a67d40b70
Aidana <at33859n@pace.edu>
2017-06-08 17:53:23 +0600

    First commit: moving label, textfield and submit button

--- AnotherTry/ViewController.swift
+++ AnotherTry/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //data fields
+    var x = 0, y = 0
+    var winCounter = 0
+   
+    
+    @IBOutlet weak var equation: UILabel!
+    @IBOutlet weak var line: UIButton!
+    @IBOutlet weak var userInput: UITextField!
+ 
+    @IBOutlet weak var Score: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newQuestion()
+        Timer.scheduledTimer(timeInterval: 0.2,
+                                          target: self,
+                                          selector: #selector(moveQuestionLabel),
+                                          userInfo: nil,
+                                          repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Moving Label
+    
+    func moveQuestionLabel() {
+        if(equation.center.y >= line.center.y) {
+            newQuestion()
+        }
+        UILabel.animate(withDuration: 0.2) {
+            self.equation.center.y += CGFloat(5)
+        }
     }
-
-
+    
+    // MARK: Generate new random question
+    
+    func newQuestion() {
+       x = Int(arc4random_uniform(9) + 1)
+       y = Int(arc4random_uniform(9) + 1)
+        equation.text = "\(x) x \(y) ="
+        equation.center.y = 50
+        
+    }
+    
+    //create a textfield and a submit button. If the user enters answer into textfield and presses submit button, the application should check if the answer is either right or wrong.
+    
+    @IBAction func SubmitButton(_ sender: AnyObject) {
+        let answer = Int(userInput.text!)
+        let rightAns = Int(x * y)
+        if answer == rightAns {
+            winCounter += 1
+            Score.text = "\(winCounter)"
+        }
+        else{
+            winCounter -= 1
+            Score.text = "\(winCounter)"
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
 }
+
+
+
 
